def72289271f62a858c01218cd544ce752c0e90b
Snigdhaman Chatterjee <snigdhaman1991@gmail.com>
2016-05-05 20:31:25 +0530

    JSON serialization demo

--- JSON/ViewController.swift
+++ JSON/ViewController.swift
@@ -12,7 +12,21 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = NSURL(string: "https://freegeoip.net/json/")
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url!) { (data, response, error) -> Void in
+            if error != nil {
+                print("Error!!")
+            } else if let urlContent = data {
+                do {
+                    let jsonResult = try NSJSONSerialization.JSONObjectWithData(urlContent, options: NSJSONReadingOptions.MutableContainers)
+                    print(jsonResult["city"])
+                } catch {
+                    print("Json Error")
+                }
+            }
+        }
+        task.resume()
     }
 
     override func didReceiveMemoryWarning() {
